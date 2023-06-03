4c0f3cbc5849ee10dc0dbfab1e70b37fd7ead67d
Jhonni test <Jhonni888@yandex.ru>
2023-06-03 15:25:02 +0300

    operator bonus code well done

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,29 @@
 public class Main {
     public static void main(String[] args) {
 
+        int balance = 100;
+        int pay = 1700;
+        int popolnenie = balance + pay;
+        int bonus = popolnenie / 100;
+
+        if (popolnenie > 1000) {
+            System.out.println("Итоговый счёт с бонусом составляет: ");
+            System.out.println(bonus + popolnenie);
+            System.out.println("Начислено бонусов: ");
+            System.out.println(bonus);
+        } else {
+            System.out.println("Итоговый счёт составляет: ");
+            System.out.println(balance + pay);
+            System.out.println("Извините бонусы не зачислены, ждём полной оплаты");
+            System.out.println("Ваш оператор ООО 'STOLEN'");
+        }
         // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
 
-        System.out.println("Премиальные мили составляют:");
-        int priceTicket = 1000;
-        int money = 20;
-        int bonus;
-
-        System.out.println(bonus = priceTicket / money);
-
-        // параметров программы: одну для хранения
-        // стоимости билета, другую для хранения количества
-        // рублей для одной бонусной мили
-
-        // Рассчитываете количество бонусных миль, используя
-        // значения заведённых переменных. Ответ сохраняете в
-        // новую переменную и выводите на экран
+        // Условным оператором проверяете, превысила ли
+        // сумма пополнения порог, и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
     }
 }
